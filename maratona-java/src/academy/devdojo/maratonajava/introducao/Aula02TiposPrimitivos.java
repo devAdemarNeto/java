16a114f78128af3d688b5319b22db5622dfa341b
Ademar Neto <ademarnetodev@gmail.com>
2025-07-02 06:06:48 -0300

    first commit

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        String nome = "Goku";
+
+
+
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println(caractere);
+        System.out.println("Oi eu sou "+nome);
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Ademar";
+        String endereco = "Tv N5, 397";
+        double salario = 2000;
+        String data = "02/07/2025";
+        String relatorio = "Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salario de "+salario+", na data "+data;
+
+        System.out.println(relatorio);
+
+    }
+
+}
