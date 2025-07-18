743eafb682a4337bf4b30801bdc2efb03e0fb5e3
Ademar Neto <ademarnetodev@gmail.com>
2025-07-17 21:01:15 -0300

    orientação a objeto

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -1,11 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpresoraEstudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
+        ImpresoraEstudante impressora = new ImpresoraEstudante();
 
         estudante01.nome = "Midorya";
         estudante01.idade = 15;
@@ -15,12 +17,10 @@ public class EstudanteTest01 {
         estudante02.idade = 16;
         estudante02.sexo = 'F';
 
-        System.out.println(estudante01.nome);
-        System.out.println(estudante01.sexo);
-        System.out.println(estudante01.idade);
 
-        System.out.println(estudante02.nome);
-        System.out.println(estudante02.sexo);
-        System.out.println(estudante02.idade);
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
+
+
     }
 }
