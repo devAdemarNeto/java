64402fd8af69880944938f0ec925c6344f392308
Ademar Neto <ademarnetodev@gmail.com>
2025-07-24 22:42:27 -0300

    introdução java

---
+++ IntroducaoJavaLogicaBasica/src/Aula1Naruto.java
@@ -0,0 +1,6 @@
+public class Aula1Naruto {
+    public static void main(String[] args) {
+        System.out.println("Kakashi-sensei: Bem-vindo à vila da Folha, jovem programador");
+        System.out.println("Ademar:Eu vou me tornar Hokague d progrmação!!");
+    }
+}
---
+++ IntroducaoJavaLogicaBasica/src/RelatorioMissao.java
@@ -0,0 +1,10 @@
+public class RelatorioMissao {
+    public static void main(String[] args) {
+        System.out.println("Ninja: Ademar");
+        System.out.println("Vila: Konoha");
+        System.out.println("Missão: Aula 1 java");
+        System.out.println("Status: concluida");
+        System.out.println("Frase: Nunca desista, é o meu jeito ninja");
+
+    }
+}
