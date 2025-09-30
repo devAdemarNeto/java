e17b981348bfe83cbe392000c9e1bfd29bb050d2
Ademar Neto <ademarnetodev@gmail.com>
2025-09-30 16:21:21 -0300

    Refatoracao

--- aprendendo-spring/src/main/java/com/javanauta/aprendendospring/AprendendoSpringApplication.java
+++ aprendendo-spring/src/main/java/com/javanauta/aprendendospring/AprendendoSpringApplication.java
@@ -1,5 +1,6 @@
 package com.javanauta.aprendendospring;
 
+import com.javanauta.aprendendospring.infrastructure.entity.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +10,7 @@ public class AprendendoSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AprendendoSpringApplication.class, args);
 
-		Usuario joao = new Usuario("Joao", "joaodasilva@hotmail.com", "123456" );
+
 
 		
 	}
