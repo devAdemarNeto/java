bcb606576015125606fceae4c94836eaf1321cb6
Ademar Neto <ademarnetodev@gmail.com>
2025-10-03 00:04:52 -0300

    aprendendo spring- Spring Data

---
+++ aprendendo-spring-2/src/main/java/com/ademar/aprendendospring2/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.ademar.aprendendospring2.infrastructure.repository;
+
+import com.ademar.aprendendospring2.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ aprendendo-spring-2/src/main/java/com/ademar/aprendendospring2/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.ademar.aprendendospring2.infrastructure.repository;
+
+import com.ademar.aprendendospring2.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
